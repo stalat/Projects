ff118e2e97bcec17c63697d742f97671efbe6968
Taps1 <talatparwez2@gmail.com>
2018-07-02 16:34:48 +0530

    'first view request/response generated

--- django_projects/awa/apps/boards/views.py
+++ django_projects/awa/apps/boards/views.py
@@ -2,5 +2,9 @@
 from __future__ import unicode_literals
 
 from django.shortcuts import render
+from django.http import HttpResponse
+
+def home(request):
+    return HttpResponse('Hello, World!')
 
 # Create your views here.
