15dacdeebb5a350d3d9847f581c756f41908d8d6
Taps1 <talatparwez2@gmail.com>
2018-07-02 22:37:34 +0530

    response generated from database values

--- django_projects/awa/apps/boards/views.py
+++ django_projects/awa/apps/boards/views.py
@@ -3,8 +3,17 @@ from __future__ import unicode_literals
 
 from django.shortcuts import render
 from django.http import HttpResponse
+from models import Board
 
 def home(request):
-    return HttpResponse('Hello, World!')
+    boards = Board.objects.all()
+    boards_name = list()
+
+    for board in boards:
+        boards_name.append(board.name)
+
+    response_html = "<br>".join(boards_name)
+
+    return HttpResponse(response_html)
 
 # Create your views here.
