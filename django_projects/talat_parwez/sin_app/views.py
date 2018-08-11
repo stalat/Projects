9d92918fbbfa560b21120f251887067a86131b5f
Taps1 <talatparwez2@gmail.com>
2018-08-12 01:43:40 +0530

    profile image takem from  models

--- django_projects/talat_parwez/sin_app/views.py
+++ django_projects/talat_parwez/sin_app/views.py
@@ -2,7 +2,7 @@
 from __future__ import unicode_literals
 
 from django.shortcuts import render
-from models import Portfolio
+from models import Portfolio, UserProfile
 from django.views.generic import TemplateView, FormView
 
 # Create your views here.
@@ -12,4 +12,5 @@ class Home(TemplateView):
 
     def get(self, request):
     	images = Portfolio.objects.all()
-        return render(request, self.template_name, {'portfolio': images})
+        user_profile = UserProfile.objects.all()[0]
+        return render(request, self.template_name, {'portfolio': images, 'user': user_profile})
