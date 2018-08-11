d82b03522ca20c2fd0784261d8dbca946c7ee526
Taps1 <talatparwez2@gmail.com>
2018-08-11 14:33:14 +0530

    model created & registered on admin panel with image field

--- django_projects/talat_parwez/sin_app/models.py
+++ django_projects/talat_parwez/sin_app/models.py
@@ -7,18 +7,18 @@ from django.db.models.signals import post_save
 
 # Create your models here.
 
-Class UserProfile(models.Model):
+class UserProfile(models.Model):
     user = models.OneToOneField(User)
     description = models.CharField(max_length=100, default='')
     city = models.CharField(max_length=100, default='')
     website = models.URLField(default='')
     phone = models.IntegerField(default=0)
-    image = models.ImageField()
+    image = models.ImageField(upload_to='profile_image', blank=True)
 
     def __str__(self):
         return self.user.username
 
-    def create_profile(sender, **kargs):
+    def create_profile(sender, **kwargs):
         if kwargs['created']:
             user_profile = UserProfile.objects.create(user=kwargs['instance'])
 
