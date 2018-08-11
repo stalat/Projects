28b1e65a42e15cc2cfe62c269db04a41576035b6
Taps1 <talatparwez2@gmail.com>
2018-08-11 17:15:59 +0530

    image object sent to HTML page

--- django_projects/talat_parwez/sin_app/views.py
+++ django_projects/talat_parwez/sin_app/views.py
@@ -2,6 +2,7 @@
 from __future__ import unicode_literals
 
 from django.shortcuts import render
+from models import Portfolio
 from django.views.generic import TemplateView, FormView
 
 # Create your views here.
@@ -10,4 +11,5 @@ class Home(TemplateView):
     template_name = 'index.html'
 
     def get(self, request):
-        return render(request, self.template_name)
+    	images = Portfolio.objects.all()
+        return render(request, self.template_name, {'portfolio': images})
