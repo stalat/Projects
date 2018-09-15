e79efd1e427a185e745a336f33ee0c9df091e24b
Taps1 <talatparwez2@gmail.com>
2018-09-16 04:05:07 +0530

    refactoration and requirements.txt added

--- Assignments/NewsTechAssignment.py
+++ Assignments/NewsTechAssignment.py
@@ -18,22 +18,31 @@ converter = html2text.HTML2Text()
 # CleanUp process is been done in further steps
 text = converter.handle(html_content.text)
 content_without_urls = re.sub(r'http\S+', '', text)
-content_without_sp_char = re.sub("[^A-Za-z0-9.\']+", " ", content_without_urls)
 desc_confidence_score = {}
 comprehend = boto3.client(service_name='comprehend', region_name='us-east-1')
 
-# Here, I have iterated over the content splitted by '.' as comprehend API wasn't able to
-# process the huge data at once due to it's limit for 5000 bytes. May need to explore to improve
-# the performance
-for item in content_without_sp_char.split('.'):
-    if len(item) == 0:
-        continue
 
-    # Here, I have used comprehend API provided by AWS
-    interm_result = comprehend.detect_key_phrases(Text=item, LanguageCode='en')
-    for val in interm_result.get('KeyPhrases'):
-        # setting up the key-phrased along with it's score
-        desc_confidence_score[val.get('Text')] =  val.get('Score')
+def get_score(text_content):
+    """
+    Here, I have iterated over the content splitted by '.' as comprehend API wasn't able to
+    process the huge data at once due to it's limit for 5000 bytes. May need to explore to improve
+    the performance
+    """
+    for item in text_content.split('.'):
+        if len(item) == 0:
+            continue
 
-# Showing up the final result with descending score for key-phrases
-print("Final result is: "), sorted(desc_confidence_score.items(), key=lambda x: x[1], reversed=True)
+        # Here, I have used comprehend API provided by AWS
+        interm_result = comprehend.detect_key_phrases(Text=item, LanguageCode='en')
+        for val in interm_result.get('KeyPhrases'):
+            # setting up the key-phrased along with it's score
+            desc_confidence_score[val.get('Text')] =  val.get('Score')
+
+        # Showing up the final result with descending score for key-phrases
+    return sorted(desc_confidence_score.items(), key=lambda x: x[1], reverse=True)
+
+
+content_without_sp_char = re.sub("[^A-Za-z0-9.\']+", " ", content_without_urls)
+resultant = get_score(content_without_sp_char)
+for res in resultant:
+    print(res)
