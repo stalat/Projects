821003d5dc57e709a08e8a6b0b289ede8784a5bc
Taps1 <talatparwez2@gmail.com>
2021-06-12 14:42:11 +0530

    added first view function

--- django_projects/practice_django/testApp/views.py
+++ django_projects/practice_django/testApp/views.py
@@ -1,3 +1,10 @@
-from django.shortcuts import render
+# python level imports
+import datetime
 
-# Create your views here.
+# django level imports
+from django.shortcuts import render
+from django.http import HttpResponse
+
+def welcome(request):
+	s = "<h1>Welcome to Django classes. {0}</h1>".format(str(datetime.datetime.now()))
+	return HttpResponse(s)
