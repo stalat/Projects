dee345ef27a0d969e3b4d3d8b713e48c5aabc7ee
Taps1 <talatparwez2@gmail.com>
2021-06-12 14:38:35 +0530

    added a URL for welcome page and corresponding views

--- django_projects/practice_django/practice_django/urls.py
+++ django_projects/practice_django/practice_django/urls.py
@@ -15,7 +15,10 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
+from testApp import urls
+from testApp.views import welcome
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('hello/', welcome),
 ]
