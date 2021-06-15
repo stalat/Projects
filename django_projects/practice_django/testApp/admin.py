a314eeb359ba85af3e01ebdd2e12d8e657c3cb4f
Taps1 <talatparwez2@gmail.com>
2021-06-15 17:31:07 +0530

    models are created & registered to admin panel

--- django_projects/practice_django/testApp/admin.py
+++ django_projects/practice_django/testApp/admin.py
@@ -1,3 +1,7 @@
 from django.contrib import admin
+from testApp.models import  Student
 
-# Register your models here.
+class StudentAdmin(admin.ModelAdmin):
+	list_display = ['name', 'marks']
+
+admin.site.register(Student, StudentAdmin)
