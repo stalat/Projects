6188024a1658e989f0b1abb606f09b02f245c738
Taps1 <talatparwez2@gmail.com>
2021-06-23 22:19:53 +0530

    Thank you page is displayed after submitting the Django form

--- django_projects/practice_django/testApp/views.py
+++ django_projects/practice_django/testApp/views.py
@@ -29,11 +29,16 @@ def studentRegistration(request):
 	form = StudentRegistrationForm()
 	return render(request, 'testApp/student_registration.html', {'form': form})
 
+def thankyou_view(request):
+	return render(request, 'testApp/thank_you.html')
+
 def studentFeedback(request):
 	form = StudentFeedbackForm()
 	if request.method == "POST":
 		form = StudentFeedbackForm(request.POST)
 		if form.is_valid():
 			print("Form validation success, printing feedback information")
-			print(form.cleaned_data)
+			# print(form.cleaned_data)
+			return thankyou_view(request)
+			
 	return render(request, 'testApp/student_feedback.html', {'form': form})
