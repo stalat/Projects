edcdcb0fb028ce6b65fd66f4618b33a8dd9c85bd
Taps1 <talatparwez2@gmail.com>
2021-06-25 21:04:31 +0530

    Bot is restricted to submit the form

--- django_projects/practice_django/testApp/forms.py
+++ django_projects/practice_django/testApp/forms.py
@@ -18,6 +18,7 @@ class StudentFeedbackForm(forms.Form):
 	rpassword = forms.CharField(label='Password (again)', widget=forms.PasswordInput)
 	feedback = forms.CharField(widget=forms.Textarea, validators=[validators.MaxLengthValidator(40), 
 		validators.MinLengthValidator(10)])
+	bot_handler = forms.CharField(required=False, widget=forms.HiddenInput)
 	
 	# def clean_name(self):
 	# 	inputname = self.cleaned_data['name']
@@ -27,6 +28,10 @@ class StudentFeedbackForm(forms.Form):
 
 	def clean(self):
 		cleaned_data = super(StudentFeedbackForm, self).clean()
+		import pdb;pdb.set_trace()
+		inputbot = cleaned_data['bot_handler']
+		if len(inputbot) > 0:
+			raise forms.ValidationError('Bot send this request')
 		inputname = cleaned_data['name']
 		if len(inputname) < 4:
 			raise forms.ValidationError('Name should atleast be 4 characters')
