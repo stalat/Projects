cb0c83c77fdad6f4f3de24d288f4196be5d7c850
Taps1 <talatparwez2@gmail.com>
2021-06-25 19:37:34 +0530

    Writing our own custom validations

--- django_projects/practice_django/testApp/forms.py
+++ django_projects/practice_django/testApp/forms.py
@@ -5,11 +5,16 @@ class StudentRegistrationForm(forms.Form):
 	name = forms.CharField()
 	marks = forms.IntegerField()
 
+def starts_with_t(value):
+    if value[0] != 't':
+        raise forms.ValidationError('Name should start with t')
+
 class StudentFeedbackForm(forms.Form):
-	name = forms.CharField()
+	name = forms.CharField(validators=[starts_with_t])
 	rollno = forms.IntegerField()
 	email = forms.EmailField()
-	feedback = forms.CharField(widget=forms.Textarea, validators=[validators.MaxLengthValidator(40)])
+	feedback = forms.CharField(widget=forms.Textarea, validators=[validators.MaxLengthValidator(40), 
+		validators.MinLengthValidator(10)])
 	
 	def clean_name(self):
 		inputname = self.cleaned_data['name']
