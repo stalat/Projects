2fd40aac613a3e319cada057098aa413027d360e
Taps1 <talatparwez2@gmail.com>
2021-06-25 19:53:55 +0530

    Single clean method for whole form validation

--- django_projects/practice_django/testApp/forms.py
+++ django_projects/practice_django/testApp/forms.py
@@ -10,14 +10,29 @@ def starts_with_t(value):
         raise forms.ValidationError('Name should start with t')
 
 class StudentFeedbackForm(forms.Form):
-	name = forms.CharField(validators=[starts_with_t])
+	# name = forms.CharField(validators=[starts_with_t])
+	name = forms.CharField()
 	rollno = forms.IntegerField()
 	email = forms.EmailField()
 	feedback = forms.CharField(widget=forms.Textarea, validators=[validators.MaxLengthValidator(40), 
 		validators.MinLengthValidator(10)])
 	
-	def clean_name(self):
-		inputname = self.cleaned_data['name']
+	# def clean_name(self):
+	# 	inputname = self.cleaned_data['name']
+	# 	if len(inputname) < 4:
+	# 		raise forms.ValidationError('Name should atleast be 4 characters')
+	# 	return inputname
+
+	def clean(self):
+		print("Validating whole form")
+		cleaned_data = super(StudentFeedbackForm, self).clean()
+		print(cleaned_data)
+		inputname = cleaned_data['name']
 		if len(inputname) < 4:
 			raise forms.ValidationError('Name should atleast be 4 characters')
-		return inputname
+
+		inputfeedback = cleaned_data['feedback']
+		if 'feedback' not in inputfeedback:
+			raise forms.ValidationError('Feedback keyword is expected')
+
+		return cleaned_data
