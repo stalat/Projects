f5d11823ffbe2a693671e98c3069cf59b85463bc
Taps1 <talatparwez2@gmail.com>
2021-09-05 17:45:07 +0530

    added serializer & added basic get/put/.. operations

--- django_projects/admin/products/views.py
+++ django_projects/admin/products/views.py
@@ -1,3 +1,21 @@
 from django.shortcuts import render
-
+from rest_framework import viewsets
+from .serializers import ProductSerializer
 # Create your views here.
+
+
+class ProductViewSet(viewsets.ViewSet):
+    def list(self, request):  # /api/products
+        pass
+
+    def create(self, request):  # /api/products/<str:id>
+        pass
+
+    def retrieve(self, request, pk=None):  # /api/products/<str:id>
+        pass
+
+    def update(self, request, pk=None):  # /api/products/<str:id>
+        pass
+
+    def destroy(self, request, pk=None):  # /api/products/<str:id>
+        pass
