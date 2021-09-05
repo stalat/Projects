792de23581b77b2ce918cf7a6cbae80df3e8c9eb
Taps1 <talatparwez2@gmail.com>
2021-09-05 17:51:13 +0530

    Implemented GET api for Products

--- django_projects/admin/products/views.py
+++ django_projects/admin/products/views.py
@@ -1,12 +1,16 @@
 from django.shortcuts import render
 from rest_framework import viewsets
+from rest_framework.response import Response
 from .serializers import ProductSerializer
+from .models import Product
 # Create your views here.
 
 
 class ProductViewSet(viewsets.ViewSet):
     def list(self, request):  # /api/products
-        pass
+        products = Product.objects.all()
+        serializer = ProductSerializer(products, many=True)
+        return Response(serializer.data)
 
     def create(self, request):  # /api/products/<str:id>
         pass
