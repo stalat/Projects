cd2d0d2cf8a2c081a9d2af2dfc4dea37fe2dac47
stalat <talatparwez27@gmail.com>
2022-12-11 22:42:36 +0530

    fix, attempt 2

--- django_projects/talat_parwez/wsgi.py
+++ django_projects/talat_parwez/wsgi.py
@@ -13,8 +13,8 @@ import site
 # Add the site-packages of the chosen virtualenv to work with
 #site.addsitedir('/var/www/.virtualenvs/exampleenv/local/lib/python2.7/site-packages')
 # Add the app's directory to the PYTHONPATH
-sys.path.append('/path/to/project/')
-os.environ['DJANGO_SETTINGS_MODULE'] = 'settings'
+# sys.path.append('/path/to/project/')
+# os.environ['DJANGO_SETTINGS_MODULE'] = 'settings'
 # Activate your virtual env
 #activate_env=os.path.expanduser("/var/www/.virtualenvs/exampleenv/bin/activate_this.py")
 #execfile(activate_env, dict(__file__=activate_env))
