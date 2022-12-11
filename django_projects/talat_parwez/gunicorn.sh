7152e256d0caefe4ba4d8be66ec12f0d1b8696d6
stalat <talatparwez27@gmail.com>
2022-12-11 20:35:24 +0530

    Django steps in gunicorn file and start the services

--- django_projects/talat_parwez/gunicorn.sh
+++ django_projects/talat_parwez/gunicorn.sh
@@ -1,3 +1,21 @@
 #!/bin/bash
 
 source venv/bin/activate
+cd /var/lib/jenkins/workspace/django-cicd/django_projects/talat_parwez
+
+python3 manage.py makemigrations
+python3 manage.py migrate
+
+echo "Migrations done"
+
+# to activate the virtual environment
+cd /var/lib/jenkins/workspace/django-cicd/django_projects/
+
+sudo cp -rf gunicorn.socket /etc/systemd/system/
+sudo cp -rf gunicorn.service /etc/systemd/system/
+
+echo "$USER"
+echo "$PWD"
+
+sudo systemctl daemon-reload
+sudo systemctl start gunicorn
