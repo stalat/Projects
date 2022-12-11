caa24695c174532478ea1c1020467f6cc2b6224c
stalat <talatparwez27@gmail.com>
2022-12-11 19:00:20 +0530

    environment file created

---
+++ django_projects/talat_parwez/envsetup.sh
@@ -0,0 +1,26 @@
+#!/bin/bash
+
+# For virtual environment
+if [-d "env"]
+then
+  echo "Python virtual env exists"
+else
+  python3 -m venv venv
+fi
+
+echo $PWD
+source venv/bin/activate
+
+pip install -r requirements.txt
+
+# For log files
+if [-d "logs"]
+then
+  echo "Log folder exists"
+else
+  mkdir logs
+  touch logs/error.log logs/access.log
+fi
+
+sudo chmod -R 777 logs
+echo "Environment setup is finished"
