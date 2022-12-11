643d5b9e2dd419493bae77fbecbe026abe08e6f8
stalat <talatparwez27@gmail.com>
2022-12-11 20:58:37 +0530

    Making gunicorn related changes

--- django_projects/talat_parwez/gunicorn.sh
+++ django_projects/talat_parwez/gunicorn.sh
@@ -9,7 +9,7 @@ python3 manage.py migrate
 echo "Migrations done"
 
 # to activate the virtual environment
-cd /var/lib/jenkins/workspace/django-cicd/django_projects/
+cd /var/lib/jenkins/workspace/django-cicd/django_projects/talat_parwez
 
 sudo cp -rf gunicorn.socket /etc/systemd/system/
 sudo cp -rf gunicorn.service /etc/systemd/system/
