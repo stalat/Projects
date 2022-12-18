14a800b3d58b550c24374a180212ae20a21098a1
stalat <talatparwez27@gmail.com>
2022-12-18 15:37:55 +0530

    fixing static files

--- django_projects/talat_parwez/urls.py
+++ django_projects/talat_parwez/urls.py
@@ -21,4 +21,5 @@ from django.conf.urls.static import static
 urlpatterns = [
     url(r'^admin/', admin.site.urls),
     url('', include('sin_app.urls', namespace='sin_app')),
-] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT) 
++ static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
